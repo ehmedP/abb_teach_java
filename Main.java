5944f79c3d46a0aa8fc868ba19f4d524e150af4c
Ahmad Ahmadov <ehmedliehmed0105@gmail.com>
2026-06-23 03:19:12 +0400

    init

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int primitiveNumber = 5;
+
+        // Autoboxing
+        Integer wrapperNumber = primitiveNumber;
+
+        Integer anotherWrapperNumber = 10;
+
+        // Unboxing
+        int anotherPrimitiveNumber = anotherWrapperNumber;
+
+    }
+
+}
